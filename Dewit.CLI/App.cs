1fcf7e60cea90457cffe7e7904a7e8e99c079e21
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-12 16:55:14 +0530

    Configure Serilog and add log statements

--- Dewit.CLI/App.cs
+++ Dewit.CLI/App.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Dewit.CLI.Data;
 using Dewit.CLI.Models;
 using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace Dewit.CLI
 {
@@ -21,22 +22,25 @@ namespace Dewit.CLI
 		{
 			List<string> acceptedArgs = new List<string>() { "now", "later", "done", "list" };
 
-			if (null != args[0] && acceptedArgs.Contains(args[0]))
+			if (args.Length > 0 && null != args[0] && acceptedArgs.Contains(args[0]))
 			{
 				if (args[0] == "now" || args[0] == "later")
 				{
-					Console.WriteLine($"Adding task : {args[1]}");
+					Log.Debug($"Adding a new task : {args[1]}");
 					var newTask = new TaskItem { Id = 1, TaskDescription = args[1], AddedOn = DateTime.Now };
 					_repository.AddTask(newTask);
+					Console.WriteLine($"Added task : {args[1]}");
 				}
 
 				if (args[0] == "done")
 				{
+					Log.Debug($"Setting status of task [{args[1]}] to Completed");
 					Console.WriteLine($"Completed task : {args[1]}");
 				}
 
 				if (args[0] == "list")
 				{
+					Log.Debug("Listing all tasks");
 					Console.WriteLine("Showing all tasks");
 					var tasks = _repository.GetTasks();
 					foreach (var task in tasks)
@@ -44,6 +48,11 @@ namespace Dewit.CLI
 						Console.WriteLine($"[{task.AddedOn}] {task.TaskDescription}");
 					}
 				}
+			}
+			else
+			{
+				Log.Error("Invalid arguments passed. Exiting.");
+				Console.WriteLine("Invalid usage.");
 			}
 		}
 	}
