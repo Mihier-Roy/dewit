684ef1380410e9366c2833ff03d291061dd68a02
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 16:37:45 +0530

    Allow sorting by status or date

--- Dewit.CLI/Commands/GetTasksCommand.cs
+++ Dewit.CLI/Commands/GetTasksCommand.cs
@@ -14,14 +14,22 @@ namespace Dewit.CLI.Commands
 
 		public GetTasksCommand(ITaskRepository repository, string name, string description = null) : base(name, description)
 		{
+			var sortOptions = new Option<string>("--sort", "Sort tasks by status or date").FromAmong("status", "date");
+			AddOption(sortOptions);
 			Handler = CommandHandler.Create<string>(GetTasks);
 			_repository = repository;
 		}
 
-		private void GetTasks(string obj)
+		private void GetTasks(string sort)
 		{
 			Log.Debug("Showing all tasks");
 			var tasks = _repository.GetTasks().Where(p => p.AddedOn > DateTime.Today.AddDays(-1));
+
+			if (sort == "status")
+				tasks = tasks.OrderBy(p => p.Status);
+			else
+				tasks = tasks.OrderBy(p => p.AddedOn);
+
 
 			Output.WriteTable(new string[] { "ID", "Task", "Status", "AddedOn", "CompletedOn" }, tasks);
 		}
