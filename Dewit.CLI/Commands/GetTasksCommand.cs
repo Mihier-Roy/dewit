9cf59776731adee09037b7078f0363461fed7f11
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 17:09:50 +0530

    Added option to filter tasks by status

--- Dewit.CLI/Commands/GetTasksCommand.cs
+++ Dewit.CLI/Commands/GetTasksCommand.cs
@@ -14,18 +14,22 @@ namespace Dewit.CLI.Commands
 
 		public GetTasksCommand(ITaskRepository repository, string name, string description = null) : base(name, description)
 		{
-			var sortOptions = new Option<string>("--sort", "Sort tasks by status or date. Default is <date>").FromAmong("status", "date");
+			var sortOptions = new Option<string>("--sort", "Sort tasks by status or date. Default is <date>")
+									.FromAmong("status", "date");
 			var durationOptions = new Option<string>("--duration", "Show tasks between the specified duration. Default is set to <today>.")
 									.FromAmong("all", "yesterday", "today", "week", "month");
+			var statusOptions = new Option<string>("--status", "Show tasks of specified status.")
+									.FromAmong("doing", "done", "later");
 			AddOption(sortOptions);
 			AddOption(durationOptions);
-			Handler = CommandHandler.Create<string, string>(GetTasks);
+			AddOption(statusOptions);
+			Handler = CommandHandler.Create<string, string, string>(GetTasks);
 			_repository = repository;
 		}
 
-		private void GetTasks(string sort = "date", string duration = "all")
+		private void GetTasks(string sort = "date", string duration = "all", string status = null)
 		{
-			Log.Debug($"Showing all tasks with arguments -> sort: {sort}, duration : {duration}");
+			Log.Debug($"Showing all tasks with arguments -> sort: {sort}, duration : {duration}, status: {status}");
 			var tasks = _repository.GetTasks();
 
 			switch (duration)
@@ -45,12 +49,25 @@ namespace Dewit.CLI.Commands
 				case "all": break;
 			}
 
+			switch (status)
+			{
+				case "doing":
+					tasks = tasks.Where(p => p.Status == "Doing");
+					break;
+				case "done":
+					tasks = tasks.Where(p => p.Status == "Done");
+					break;
+				case "later":
+					tasks = tasks.Where(p => p.Status == "Later");
+					break;
+			}
+
 			if (sort == "status")
 				tasks = tasks.OrderBy(p => p.Status);
 			else
 				tasks = tasks.OrderBy(p => p.AddedOn);
 
-			Output.WriteText($"Displaying tasks using parameters -> [aqua]sort[/]: {sort}, [aqua]duration[/] : {duration}");
+			Output.WriteText($"Displaying tasks using parameters -> [aqua]sort[/]: {sort}, [aqua]duration[/] : {duration}, [aqua]status[/]: {(status == null ? "n/a" : status)}");
 			Output.WriteTable(new string[] { "ID", "Task", "Status", "AddedOn", "CompletedOn" }, tasks);
 		}
 	}
