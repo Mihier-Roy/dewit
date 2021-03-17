8fc0a2363ef80734ee704c2ffbeffbdce1eb20be
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 16:09:20 +0530

    Use Specter.Console for output formatting

---
+++ Dewit.CLI/Utils/Output.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Dewit.CLI.Models;
+using Spectre.Console;
+
+namespace Dewit.CLI.Utils
+{
+	public static class Output
+	{
+		public static void WriteText(string text)
+		{
+			AnsiConsole.Markup(text);
+		}
+
+		public static void WriteError(string text)
+		{
+			AnsiConsole.Markup($"[red]ERROR[/] : {text}");
+		}
+
+		public static void WriteTable(string[] columnNames, IEnumerable<TaskItem> data)
+		{
+			var table = new Table();
+			table.Border = TableBorder.Square;
+
+			foreach (var column in columnNames)
+			{
+				table.AddColumn(column);
+			}
+
+			foreach (var item in data)
+			{
+				table.AddRow(new string[]
+				{
+					item.Id.ToString(),
+					item.TaskDescription,
+					item.Status == "Done" ? "[green]Done[/]" : item.Status,
+					item.AddedOn.ToString("dd-MMM-yy HH:MM"),
+					item.CompletedOn == DateTime.Parse("0001-01-01") ? "" : item.CompletedOn.ToString("dd-MMM-yy HH:MM")
+				});
+			}
+
+			AnsiConsole.Render(table);
+		}
+	}
+}
