729ee349b35ea336f1bdc2a38dfb38e9fc651417
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 16:10:21 +0530

    Formated output using Specter.Console and cleaned-up logging

--- Dewit.CLI/Commands/AddTaskCommand.cs
+++ Dewit.CLI/Commands/AddTaskCommand.cs
@@ -3,6 +3,7 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using Dewit.CLI.Data;
 using Dewit.CLI.Models;
+using Dewit.CLI.Utils;
 using Serilog;
 
 namespace Dewit.CLI.Commands
@@ -35,13 +36,13 @@ namespace Dewit.CLI.Commands
 
 			if (success)
 			{
-				Log.Debug($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
-				Console.WriteLine($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
+				Log.Information($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
+				Output.WriteText($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
 			}
 			else
 			{
 				Log.Error($"Failed to add task.");
-				Console.WriteLine($"ERROR : Failed to add task. Please try again.");
+				Output.WriteError($"Failed to add task. Please try again.");
 			}
 		}
 	}
