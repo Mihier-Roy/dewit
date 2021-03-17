31acc58118d21492d451afafce14b21a8ad582ab
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 17:46:33 +0530

    Added Tags column

--- Dewit.CLI/Migrations/20210317121601_topicTags.Designer.cs
+++ Dewit.CLI/Migrations/20210317121601_topicTags.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Dewit.CLI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Dewit.CLI.Migrations
 {
     [DbContext(typeof(TaskContext))]
-    partial class TaskContextModelSnapshot : ModelSnapshot
+    [Migration("20210317121601_topicTags")]
+    partial class topicTags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
