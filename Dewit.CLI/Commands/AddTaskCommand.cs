5181178fe21416992c45654707b909746124c209
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-17 18:44:13 +0530

    Added option to add tags when using now/later

--- Dewit.CLI/Commands/AddTaskCommand.cs
+++ Dewit.CLI/Commands/AddTaskCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using System.CommandLine;
 using System.CommandLine.Invocation;
+using System.Text.RegularExpressions;
 using Dewit.CLI.Data;
 using Dewit.CLI.Models;
 using Dewit.CLI.Utils;
@@ -16,19 +17,28 @@ namespace Dewit.CLI.Commands
 		public AddTaskCommand(ITaskRepository repository, string name, string description = null) : base(name, description)
 		{
 			AddArgument(new Argument<string>("title", "Description of the task you're currently performing."));
-			Handler = CommandHandler.Create<string>(AddTask);
+			AddOption(new Option<string>("--tags", "Comma-separated list of tags to identify your task. Example : --tags work,testing"));
+			Handler = CommandHandler.Create<string, string>(AddTask);
 			_repository = repository;
 			_name = name;
 		}
 
-		private void AddTask(string title)
+		private void AddTask(string title, string tags = null)
 		{
-			Log.Debug($"Adding a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
+			if (null != tags)
+			{
+				Regex r = new Regex("(?:[^a-z0-9,_])", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+				tags = r.Replace(tags, string.Empty);
+				tags = tags[tags.Length - 1] == ',' ? tags.Remove(tags.Length - 1) : tags;
+			}
+
+			Log.Debug($"Adding a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}, Tags = {tags}");
 			var newTask = new TaskItem
 			{
 				TaskDescription = title,
 				AddedOn = DateTime.Now,
-				Status = _name == "now" ? "Doing" : "Later"
+				Status = _name == "now" ? "Doing" : "Later",
+				Tags = tags
 			};
 
 			_repository.AddTask(newTask);
@@ -36,8 +46,8 @@ namespace Dewit.CLI.Commands
 
 			if (success)
 			{
-				Log.Information($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
-				Output.WriteText($"[green]Added a new task[/] : {title}, Status = {(_name == "now" ? "Doing" : "Later")}");
+				Log.Information($"Added a new task : {title}, Status = {(_name == "now" ? "Doing" : "Later")}, Tags = {tags}");
+				Output.WriteText($"[green]Added a new task[/] : {title}, [aqua]Status[/] = {(_name == "now" ? "Doing" : "Later")}, [aqua]Tags[/] = {tags}");
 			}
 			else
 			{
