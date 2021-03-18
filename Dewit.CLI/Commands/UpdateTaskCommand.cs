1b1fa4b81f20dc36125e18d8dfff903fa0fffa61
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-18 18:46:01 +0530

    Added option to add tags to a task after it has been created

--- Dewit.CLI/Commands/UpdateTaskCommand.cs
+++ Dewit.CLI/Commands/UpdateTaskCommand.cs
@@ -15,14 +15,15 @@ namespace Dewit.CLI.Commands
 		public UpdateTaskCommand(ITaskRepository repository, string name, string description = null) : base(name, description)
 		{
 			AddArgument(new Argument<int>("id", "ID of the task you wish to update."));
-			AddOption(new Option<string>("--title", "Specify when the task was completed"));
-			Handler = CommandHandler.Create<int, string>(UpdateTaskDetails);
+			AddOption(new Option<string>("--title", "Change the description of the task."));
+			AddOption(new Option<string>("--add-tags", "Add new tag(s) to an existing task. Example --add-tags work,test"));
+			Handler = CommandHandler.Create<int, string, string>(UpdateTaskDetails);
 			_repository = repository;
 		}
 
-		private void UpdateTaskDetails(int id, string title = null)
+		private void UpdateTaskDetails(int id, string title = null, string addTags = null)
 		{
-			Log.Debug($"Modifying information of task [{id}]. Params -> Title: {title}");
+			Log.Debug($"Modifying information of task [{id}]. Params -> Title: {title}, Tags: {addTags}");
 
 			var task = _repository.GetTaskById(id);
 			if (null == task)
@@ -32,17 +33,24 @@ namespace Dewit.CLI.Commands
 				return;
 			}
 
-			// If the completed-at option is provided, parse the date entered by the user
+			// Modify the title of the task
 			if (!string.IsNullOrEmpty(title))
 				task.TaskDescription = title;
+
+			// Add tag(s) to the task
+			if (!string.IsNullOrEmpty(addTags))
+			{
+				addTags = Sanitizer.SanitizeTags(addTags);
+				task.Tags = String.Join(',', task.Tags, addTags);
+			}
 
 			_repository.UpdateTask(task);
 			var success = _repository.SaveChanges();
 
 			if (success)
 			{
-				Log.Information($"Successfully updated task : {task.Id} | {task.TaskDescription} ");
-				Output.WriteText($"[green]Successfully updated task[/] : {task.Id} | {task.TaskDescription} ");
+				Log.Information($"Successfully updated task : {task.Id} | {task.TaskDescription} | {task.Tags}");
+				Output.WriteText($"[green]Successfully updated task[/] : {task.Id} | {task.TaskDescription} | {task.Tags}");
 			}
 			else
 			{
