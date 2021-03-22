6d5788b1abe97995b1a9452a726dfabd939864d1
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2021-03-22 14:05:10 +0530

    Added option to export data in JSON format

--- Dewit.CLI/Commands/ExportTasksCommand.cs
+++ Dewit.CLI/Commands/ExportTasksCommand.cs
@@ -15,25 +15,47 @@ namespace Dewit.CLI.Commands
 
 		public ExportTasksCommand(ITaskRepository repository, string name, string description = null) : base(name, description)
 		{
-			AddOption(new Option<FileInfo>("--file-path", "Path to where the exported data is to be saved. By default, it will be saved in the current directory."));
-			Handler = CommandHandler.Create<FileSystemInfo>(ExportTasks);
+			var filePathOption = new Option<FileInfo>("--path", "Path to where the exported data is to be saved. By default, it will be saved in the current directory.");
+			var formatOption = new Option<string>("--format", "Data format in which the exported data is to be saved. Default format is JSON.")
+									.FromAmong("csv", "json");
+			AddOption(filePathOption);
+			AddOption(formatOption);
+			Handler = CommandHandler.Create<FileSystemInfo, string>(ExportTasks);
 			_repository = repository;
 		}
 
-		private void ExportTasks(FileSystemInfo exportPath = null)
+		private void ExportTasks(FileSystemInfo path = null, string format = "json")
 		{
-			if (null == exportPath)
+
+			if (null == path)
 			{
-				exportPath = new FileInfo(Directory.GetCurrentDirectory());
+				path = new FileInfo(Directory.GetCurrentDirectory());
 			}
 
-			string filePath = Path.Combine(exportPath.ToString(), "dewit_tasks.csv");
+			string filePath = Path.Combine(path.ToString(), $"dewit_tasks.{format}");
 
-			Log.Debug($"Exporting all tasks to file. Path : {exportPath}");
+			Log.Debug($"Exporting all tasks to file. Format: {format}, Path : {path}");
 			var tasks = _repository.GetTasks().OrderBy(p => p.AddedOn);
 
-			FormatData.ToCsv(tasks, filePath);
-			Output.WriteText($"[green]Successfully exported tasks[/]. Data exported to : {filePath}");
+			try
+			{
+				if (format == "csv")
+				{
+					FormatData.ToCsv(tasks, filePath);
+				}
+				else
+				{
+					FormatData.ToJson(tasks, filePath);
+				}
+
+				Output.WriteText($"[green]Succesfully exported data.[/] Path : {filePath}");
+			}
+			catch (Exception ex)
+			{
+				Log.Error(ex, $"Failed to export data.");
+				Output.WriteError("Failed to export data to file. Please try again.");
+			}
+
 		}
 	}
 }
