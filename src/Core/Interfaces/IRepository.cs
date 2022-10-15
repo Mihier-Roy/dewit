ca52880db2541aa9481cae65eec345be284fa4f2
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2022-10-15 07:12:49 +0100

    Replace per-entity repository interface definitions with a generic interface definition (#29)

---
+++ src/Core/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+using Dewit.Core.Entities;
+
+namespace Dewit.Core.Interfaces
+{
+	public interface IRepository<T> where T : EntityBase
+	{
+		IEnumerable<T> List();
+		T GetById(int id);
+		void Add(T item);
+		void Update(T item);
+		void Remove(T item);
+	}
+}
