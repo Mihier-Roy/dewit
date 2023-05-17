f1fb531ca18a494ad6392e8e90a62009e580c38d
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2023-05-17 19:53:55 +0100

    Configure DI for specter.console.cli

---
+++ src/Dewit.CLI/Infrastructure/TypeResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Spectre.Console.Cli;
+
+namespace Dewit.CLI.Infrastructure;
+
+// Taken from example: https://github.com/spectreconsole/spectre.console/blob/main/examples/Cli/Injection/Infrastructure/TypeResolver.cs
+public sealed class TypeResolver : ITypeResolver, IDisposable
+{
+    private readonly IServiceProvider _provider;
+
+    public TypeResolver(IServiceProvider provider)
+    {
+        _provider = provider ?? throw new ArgumentNullException(nameof(provider));
+    }
+
+    public object Resolve(Type type)
+    {
+        if (type == null)
+        {
+            return null;
+        }
+
+        return _provider.GetService(type);
+    }
+
+    public void Dispose()
+    {
+        if (_provider is IDisposable disposable)
+        {
+            disposable.Dispose();
+        }
+    }
+}
