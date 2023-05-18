35fe9bb0bd3ecb871092b3e6263789fd4527dfb6
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2023-05-18 09:07:14 +0100

    Construct path within the service rather than at the call location

--- src/Core/Services/DataConverterService.cs
+++ src/Core/Services/DataConverterService.cs
@@ -10,28 +10,32 @@ namespace Dewit.Core.Services
 	{
 		public void ToFormat(DataFormats type, string path, IEnumerable<T> items)
 		{
+			var extension = type == DataFormats.Csv ? "csv" : "json";
+			var filePath = Path.Combine(path, $"dewit_tasks.{extension}");
+			
 			if (type == DataFormats.Csv)
 			{
-				using var writer = new StreamWriter(path);
+				using var writer = new StreamWriter(filePath);
 				using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
 				csv.WriteRecords(items);
 			}
 			else if (type == DataFormats.Json)
 			{
-				string json = JsonSerializer.Serialize(items);
+				var json = JsonSerializer.Serialize(items);
 				File.WriteAllText(path, json);
 			}
 		}
 
 		public IEnumerable<T> FromFormat(DataFormats type, string path)
 		{
+			var extension = type == DataFormats.Csv ? "csv" : "json";
+			var filePath = Path.Combine(path, $"dewit_tasks.{extension}");
+			
 			if (type == DataFormats.Csv)
 			{
-				using (var reader = new StreamReader(path))
-				using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-				{
-					return csv.GetRecords<T>().ToList();
-				}
+				using var reader = new StreamReader(filePath);
+				using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+				return csv.GetRecords<T>().ToList();
 			}
 			else if (type == DataFormats.Json)
 			{
