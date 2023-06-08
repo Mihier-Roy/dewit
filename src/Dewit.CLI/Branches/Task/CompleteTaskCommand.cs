458f6751d31f3b3ff515b078d36278e1f378ad52
Mihier-Roy <31336766+Mihier-Roy@users.noreply.github.com>
2023-06-08 15:28:10 +0100

    Implement done command using spectreCLI

---
+++ src/Dewit.CLI/Branches/Task/CompleteTaskCommand.cs
@@ -0,0 +1,51 @@
+using System;
+using System.ComponentModel;
+using Dewit.CLI.Utils;
+using Dewit.Core.Interfaces;
+using Spectre.Console.Cli;
+
+namespace Dewit.CLI.Branches.Task
+{
+	public class CompleteTaskCommand : Command<CompleteTaskCommand.Settings>
+	{
+		public class Settings : CommandSettings
+		{
+			[CommandArgument(0, "<title>")]
+			[Description("ID of the task to be updated")]
+			public int Id { get; set; }
+			
+			[CommandOption("-c|--completed-at <time>")]
+			[Description("Specify when the task was completed")]
+			public string CompletedAt { get; set; }
+		}
+		
+		private readonly ITaskService _taskService;
+
+		public CompleteTaskCommand(ITaskService taskService)
+		{
+			
+			_taskService = taskService;
+		}
+ 
+		public override int Execute(CommandContext context, Settings settings)
+		{
+			try
+			{
+				_taskService.CompleteTask(settings.Id, settings.CompletedAt);
+				Output.WriteText($"[green]Completed task[/] : {settings.Id} ");
+			}
+			catch (ArgumentException e)
+			{
+				Output.WriteError($"Task with ID {settings.Id} does not exist. View all tasks with -> dewit list");
+				return -1;
+			}
+			catch (ApplicationException e)
+			{
+				Output.WriteError("Failed to set task as completed. Please try again.");
+				return -1;
+			}
+
+			return 0;
+		}
+	}
+}
